e5c5972e77411f0b5ecaf57382a302e297b0a217
D3N <dnkh86@gmail.com>
2018-08-07 13:51:22 +0300

    minor changes 2

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,49 +1,49 @@
 class ProjectsController < ApplicationController
 
-	before_action :project_finder, only: [:edit, :update, :destroy]
+  before_action :project_finder, only: [:edit, :update, :destroy]
 
-	def index
-		@projects = Project.all_projects
-	end
+  def index
+    @projects = Project.all_projects
+  end
 
-	def new
-		@project = Project.new
-	end
+  def new
+    @project = Project.new
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def create
-		@project = Project.create(project_params)
-		if @project.save
-			redirect_to action: 'index'
-		else
-			render 'projects/new'
-		end
-	end
+  def create
+    @project = Project.create(project_params)
+    if @project.save
+      redirect_to action: 'index'
+    else
+      render 'projects/new'
+    end
+  end
 
-	def update
-		@project.update(project_params)
-		if @project.save
-			redirect_to action: 'index'
-		else
-			render 'edit'
-		end
-	end
+  def update
+    @project.update(project_params)
+    if @project.save
+      redirect_to action: 'index'
+    else
+      render 'edit'
+    end
+  end
 
-	def destroy
-		@project.destroy
-		redirect_to action: 'index'
-	end
+  def destroy
+    @project.destroy
+    redirect_to action: 'index'
+  end
 
-	private
+  private
 
-	def project_params
-		params.require(:project).permit(:name)
-	end
+  def project_params
+    params.require(:project).permit(:name)
+  end
 
-	def project_finder
-		@project = Project.find(params[:id])
-	end
+  def project_finder
+    @project = Project.find(params[:id])
+  end
 
 end
